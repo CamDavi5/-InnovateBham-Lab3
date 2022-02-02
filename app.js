04ec2d1ecfced1f1011c002ff7ba50c23a237d6e
CamDavi5 <camrdavis.biz@gmail.com>
2022-02-02 16:08:40 -0600

    added scoreboard functionality

--- app.js
+++ app.js
@@ -59,17 +59,26 @@ function checkWinner() {
 
     //Final check for a tie - forced reset
     if (turn == 9 && winner == false) {
-        console.log("The game is a TIE! Please click again to reset...");
+        //alert("The game is a TIE! Please click again to reset...");
+        tie++
+        extras[2].textContent = "Tie: " + tie;
+        extras[3].textContent = "The game is a TIE! \nPlease click again to reset..."
     }
 }
 
 //Reusable check for matching cell text content
 function checkRowColumn(c1, c2, c3) {
     if (cells[c1].textContent == 'X' && cells[c2].textContent == 'X' && cells[c3].textContent == 'X') {
-        console.log("PLAYER 1 IS THE WINNER! Good Game! Please click again to reset...")
+        //alert("PLAYER 1 IS THE WINNER! Good Game! Please click again to reset...")
+        player1++
+        extras[0].textContent = "Player 1: " + player1;
+        extras[3].textContent = "PLAYER 1 IS THE WINNER! Good Game! \nPlease click again to reset..."
         winner = true;
     } else if (cells[c1].textContent == 'O' && cells[c2].textContent == 'O' && cells[c3].textContent == 'O') {
-        console.log("PLAYER 2 IS THE WINNER! Good Game! Please click again to reset...")
+        //alert("PLAYER 2 IS THE WINNER! Good Game! Please click again to reset...")
+        player2++
+        extras[1].textContent = "Player 2: " + player2;
+        extras[3].textContent = "PLAYER 2 IS THE WINNER! Good Game! \nPlease click again to reset..."
         winner = true;
     }
 }
@@ -79,6 +88,7 @@ function gameReset() {
     for (i=0; i<cells.length; i++) {
         cells[i].textContent = "";
     }
+    extras[3].textContent = "Game in progress..."
     turn = 1;
     isXorO = 1;
     winner = false;
@@ -91,7 +101,13 @@ for (i=0; i<cells.length; i++) {
     cells[i].addEventListener('click', cellClicked);
 }
 
+let extras = document.querySelectorAll('.container > div');
+console.log(extras);
+
 //Default checks for the Tic Tac Toe board
 let isXorO = 1;
 let turn = 1;
 let winner = false;
+let player1 = 0;
+let player2 = 0;
+let tie = 0;
